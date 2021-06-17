9b0818c4d35aa43c06c0ee3b6ceddb5b46c76e48
breadbread1984 <breadbread1984@163.com>
2021-06-17 16:31:42 +0800

    update

--- train.py
+++ train.py
@@ -4,19 +4,28 @@ import tensorflow as tf;
 from models import TextEncoder, VideoGenerator, IntrospectiveDiscriminator;
 from dataset.load_dataset import load_mnist_caption;
 
-def main(filename = None, step = 10000):
+batch_size = 4;
+
+def main(filename = None, step = 10000, val_interval = 100):
   
   (train_data, train_label), (val_data, val_label) = load_mnist_caption(filename);
-  train_data_iter = iter(train_data);
-  train_label_iter = iter(train_label);
-  val_data_iter = iter(val_data);
-  val_label_iter = iter(val_label);
+  train_data_iter = iter(train_data.batch(batch_size));
+  train_label_iter = iter(train_label.batch(batch_size));
+  val_data_iter = iter(val_data.batch(batch_size));
+  val_label_iter = iter(val_label.batch(batch_size));
+  g = VideoGenerator();
+  d = IntrospectiveDiscriminator();
   for i in range(step):
-    train_sample = next(train_data_iter);
-    train_target = next(train_label_iter);
-    val_sample = next(val_data_iter);
-    val_target = next(val_label_iter);
-
+    real = tf.transpose(next(train_data_iter),(0,1,3,4,2)); # train_sample = (batch_size, 16, 64, 64, 1)
+    caption = next(train_label_iter); # train_caption.shape = (batch_size, 9)
+    fake = g(train);
+    real_motion_disc, real_frame_disc, real_text_disc, real_recon_latent0, real_recon_latent1 = d([real, caption]);
+    fake_motion_disc, fake_frame_disc, fake_text_disc, fake_recon_latent0, fake_recon_latent1 = d([fake, caption]);
+    if i % val_interval == 0:
+      real = next(val_data_iter);
+      caption = next(val_label_iter);
+      # TODO
+    
 
 if __name__ == "__main__":
 
