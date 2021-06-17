a8f6dbcc21899c2ecd93997313f564317c9fa6fa
breadbread1984 <breadbread1984@163.com>
2021-06-17 15:54:59 +0800

    update

--- dataset/load_dataset.py
+++ dataset/load_dataset.py
@@ -11,7 +11,7 @@ def load_mnist_caption(filename):
   data_val = f['mnist_gif_val'];
   captions_val = f['mnist_captions_val'];
   f.close();
-  train_data = tf.data.Dataset.from_tesnor_slices(data_train);
+  train_data = tf.data.Dataset.from_tensor_slices(data_train);
   train_label = tf.data.Dataset.from_tensor_slices(captions_train);
   val_data = tf.data.Dataset.from_tensor_slices(data_val);
   val_label = tf.data.Dataset.from_tensor_slices(captions_val);
